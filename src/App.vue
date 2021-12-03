6eb6578c5c0d4be1ab8558c6e92ee8e23e41267d
Thino Rakotomalala <rakotomalalathino@gmail.com>
2021-12-03 11:17:04 +0100

    login creation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <div id="nav"></div>
+  <router-view />
 </template>
-
+<script>
+export default {};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="main"></div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.main {
+  display: flex;
+  width: 100%;
+  height: auto;
+}
+</style>
